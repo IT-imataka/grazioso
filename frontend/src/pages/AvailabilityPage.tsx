3b615ca671f097cf4aefa1c0d2532091c2be2386
IT-imataka <takashi6128_gakky@outlook.com>
2026-08-05 00:00:34 +0900

    add: new component

---
+++ frontend/src/pages/AvailabilityPage.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function AvailabilityPage () {
+    return (
+        <div>
+            <h1 className="">
+            <img src="/Availaibility.png" alt="Grazioso Logo" className="h-hull w-full" />
+            </h1>
+        </div>
+    )
+}
